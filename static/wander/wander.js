ffed3ab2794975f58fd321227284b87ca513cb2f
James Campbell <james.campbell@tanti.org.uk>
2026-06-01 11:51:16 +0100

    feat(static): add wander interactive page
    Adds standalone HTML page with JavaScript for wander functionality.

---
+++ static/wander/wander.js
@@ -0,0 +1,16 @@
+window.wander = {
+  // Other Wander consoles that visitors can reach from my console.
+  consoles: [
+    'https://susam.net/wander/',
+    'https://dahlstrand.net/wander/',
+    'https://www.siddharthagolu.com/wander/',
+    'https://wander.liontask.dev/',
+    'https://juleskourelakos.com/wander/',
+    'https://bitzero.cloud/wander/',
+  ],
+  // Websites and pages I recommend to other wanderers.
+  pages: [
+    'https://theref.eth.limo/',
+    // TODO: add sites you like — blogs, tools, weird corners of the web
+  ],
+}
